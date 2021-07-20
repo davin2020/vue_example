6f1c024376d17a5da99e24996f20410caad568e4
Davin <davin.hayden@outlook.com>
2021-07-20 23:55:30 +0100

    separated lucy cruise option into js file, and combined with binding colors & images

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const appCruise = Vue.createApp({
-    data() {
-        return {
-            productName: 'Book a Trip to Leith',
-            productDescription: 'Take a break from the smog of Westerley and book a trip to the verdant moon Leith, aboard Lucy the Spaceship. Watch the Killjoys in action as you leave the smog of Westerley behind.',
-            // additional properties later
+// const appCruise2 = Vue.createApp({
+//     data() {
+//         return {
+//             productName: 'Book a Trip to Leith',
+//             productDescription: 'WHY NOT Take a break from the smog of Westerley and book a trip to the verdant moon Leith, aboard Lucy the Spaceship. Watch the Killjoys in action as you leave the smog of Westerley behind.',
+//             // additional properties later
 
 
-        }
-    },
-});
+//         }
+//     },
+// });
 
 //seems this line can go in either the html or js file?
-appCruise2.mount('#quad_cruise');
+// appCruise2.mount('#quad_cruise');
 
 
 //  alternative syntax for unnamed variable/obj
@@ -34,17 +34,33 @@ appCruise2.mount('#quad_cruise');
     }).mount('#appBindTest2');
 
 
-const appLucy = Vue.createApp({
+// const appLucy = Vue.createApp({
+//     data() {
+//         return {
+//             productName: 'XXBook a Cruise to the Moon',
+//             productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+//             // additional properties later
+
+
+//         }
+//     },
+// });
+
+// //seems this line can go in either the html or js file?
+// appLucy.mount('#lucy_cruise');
+
+const lucyCruise = Vue.createApp({
     data() {
         return {
-            productName: 'XXBook a Cruise to the Moon',
-            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            productName: 'Book a Trip to Leith',
+            productDescription: 'Take a break from the smog of Westerley and book a trip to the verdant moon Leith. Relax aboard aboard Lucy the Spaceship with a glass of Hock and watch the Killjoys in action',
             // additional properties later
-
+            	imageSource2: 'spaceship_lucy.png',
+                sourceColor2: 'color:green;'
 
         }
     },
 });
 
 //seems this line can go in either the html or js file?
-appLucy.mount('#lucy_cruise');
+lucyCruise.mount('#lucy_cruise');
